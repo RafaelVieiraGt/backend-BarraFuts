e4704bf55dfdaf10fce0534a10638979218abf2c
Rafael Camilli Vieira <rafaelcamillivieira@gmail.com>
2024-01-25 17:07:04 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ const routes = require('./routes')
 const app = express()
 require('./config/mongoConfig')
 
+const port = process.env.PORT || 3333
+
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, ()=> console.log("Servidor ouvindo!"))
+app.listen(port, ()=> console.log("Servidor ouvindo!"))
